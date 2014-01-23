26e19b02fbe04791e4f31134620f3d58d06bc439
jrote1 <jrote1@me.com>
2014-01-23 10:07:02 +0000

    Updated version number

--- ObjectHydrator/Properties/AssemblyInfo.cs
+++ ObjectHydrator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle( "ObjectHydrator" )]
+[assembly: AssemblyTitle("SqlObjectHydrator")]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "ObjectHydrator" )]
+[assembly: AssemblyProduct("SqlObjectHydrator")]
 [assembly: AssemblyCopyright( "Copyright ©  2013" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
 [assembly: InternalsVisibleTo( "SqlObjectHydrator.Tests" )]
